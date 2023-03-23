2bf0b5357b6391f7cb19e344c6502304585655cc
EC2 Default User <ec2-user@ip-172-31-39-139.ap-northeast-1.compute.internal>
2023-03-23 15:03:00 +0000

    レイアウト

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -34,7 +34,7 @@ class Illustration < ApplicationRecord
   has_many :tags, through: :tag_illustrations
 
   # バリデーション
-  validates :title, presence: true, length: { maximum: 14 }
+  validates :title, presence: true, length: { maximum: 20 }
   validates :image, presence: true
   
   def save_tags(tags)
